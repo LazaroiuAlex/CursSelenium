48b1fca5c342bad0ad5de30177999a778ba4b41c
unknown <lazaroiu.alex03@gmail.com>
2024-04-15 09:19:43 +0300

    commit initial

---
+++ src/main/java/curs12/OpenBrowser.java
@@ -0,0 +1,16 @@
+package curs12;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenBrowser {
+	
+	@Test
+	public void testOpenBrowser() {
+		
+		WebDriver driver = new ChromeDriver(); // EdgeDriver sau FireFoxDriver
+		driver.get("https://keybooks.ro/");
+	}
+
+}
